5abd7cd3ecae30c74e347ed534672716ecc30a6d
wjw001 <aaaawjwa@gmail.com>
2023-05-18 09:03:57 +0800

    feat: 移除多餘內容並集成 unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
+import type { PluginOption } from 'vite'
 import { defineConfig } from 'vite'
+import unoCSS from 'unocss/vite'
 import react from '@vitejs/plugin-react'
+import {presetAttributify, presetIcons, presetUno} from "unocss";
+
+const plugins: (PluginOption | PluginOption[])[] = [
+  unoCSS({
+    shortcuts: [
+      { logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180' },
+    ],
+    presets: [
+      presetUno(),
+      presetAttributify(),
+      presetIcons({
+        extraProperties: {
+          'display': 'inline-block',
+          'vertical-align': 'middle',
+        },
+      }),
+    ],
+    inspector: true,
+  }),
+  react()
+]
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig({ plugins })
