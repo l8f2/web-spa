dd4ff07b24ba02e13b8eeec4277ba01fa33cd1c1
wjw001 <aaaawjwa@gmail.com>
2023-05-18 15:38:05 +0800

    build: 集成 antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from 'antd'
+
 function App() {
 	const n: number = 1
 
-	return <>App {n}</>
+	return (
+		<>
+			App {n} <Button type={'primary'}>ant</Button>
+		</>
+	)
 }
 
 export default App
