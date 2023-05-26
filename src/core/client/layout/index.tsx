0f1612fe095ab2d1128adf404f44f04ade4c67ea
wjw001 <aaaawjwa@gmail.com>
2023-05-26 09:39:39 +0800

    build: prettier 改為 eslint-prettier

--- src/core/client/layout/index.tsx
+++ src/core/client/layout/index.tsx
@@ -1,8 +1,8 @@
-import { TAppOpts } from '@core/utils/create-app.tsx'
-import { Fragment } from 'react'
+import { TAppOpts } from "@core/utils/create-app.tsx";
+import { Fragment } from "react";
 
 export const Layout = ({ router }: TAppOpts) => {
-	const Router = router ? router : Fragment
+  const Router = router ? router : Fragment;
 
-	return <Router></Router>
-}
+  return <Router></Router>;
+};
